ffa08b2a980c63cfe5ebfef7c01ee36ae98a7da9
kyzsu <l0calh34rtz@gmail.com>
2023-03-31 15:09:35 +0700

    feat: update authApiSlice

--- src/features/cv/CV.js
+++ src/features/cv/CV.js
@@ -1,9 +1,16 @@
 import { LogoutIcon } from "@heroicons/react/outline"
 import { PulseLoader } from "react-spinners"
 import { useSendLogoutMutation } from "../auth/authApiSlice"
+import { useEffect } from "react"
 
 const CV = () => {
-  const [sendLogout, { isLoading }] = useSendLogoutMutation()
+  const [sendLogout, { isLoading, isSuccess }] = useSendLogoutMutation()
+
+  useEffect(() => {
+    if (isSuccess) {
+      JSON.parse(localStorage.setItem("isLoggedIn", JSON.stringify(false)))
+    }
+  }, [isSuccess])
 
   if (isLoading) return <PulseLoader color="#fff" size={150} />
 
@@ -12,10 +19,10 @@ const CV = () => {
       CV
       <button
         type="button"
-        className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         onClick={sendLogout}
       >
-        <LogoutIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+        <LogoutIcon className="w-5 h-5 mr-2 -ml-1" aria-hidden="true" />
         Logout
       </button>
     </div>
